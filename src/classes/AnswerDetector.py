0456dd28639a1600428e408ae54b8398c15c84f8
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-07-23 22:28:17 -0400

    begin grid answer comprehension

--- src/classes/AnswerDetector.py
+++ src/classes/AnswerDetector.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 import numpy as np
-import math
 from classes.Grid import Grid
+import constants.answers as answer_list
 
 class AnswerDetector():
     def __init__(self, **kwargs):
-        self.grid = Grid()
+        self.save_thresh = None
 
-    def _get_circles(self, img):
+    def _get_circlish(self, img):
         copy = img.copy()
         copy = cv.cvtColor(copy, cv.COLOR_BGR2GRAY)
         zeros_copy = np.zeros_like(copy)
 
         thresh = cv.adaptiveThreshold(copy, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 15, 3)
+        self.save_thresh = thresh
         contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         contour_list = []
@@ -36,35 +37,58 @@ class AnswerDetector():
         cv.imshow("circles", zeros_copy)
 
         return points
+    
+    def _get_blobs(self, img, display=False):
+        copy = img.copy()
+        copy = cv.cvtColor(copy, cv.COLOR_BGR2GRAY)
 
-    def detect(self, img):
-        points = self._get_circles(img)
-        srt_pts = sorted(points, key=lambda  pt: pt[1])
-        grid = []
-            
+        params = cv.SimpleBlobDetector_Params()
+        params.filterByArea = True
+        params.minArea = 350
+        detector = cv.SimpleBlobDetector_create(params)
+        
+        keypoints = detector.detect(copy)
+        points = [(int(kpt.pt[0]), int(kpt.pt[1])) for kpt in keypoints]
+        
+        if display:
+            copy = cv.drawKeypoints(copy, keypoints, None, (0, 255, 0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            cv.imshow("drawn", copy)
+        
+        return points
+    
+    def _merge_cols(self, cols):
+        ret_pts = []
 
-        new_col = True
-        for x1, y1 in srt_pts: # current point
-            
+        for col in cols:
+            for pt in col:
+                ret_pts.append(pt)
 
-            for col in grid:
-                x2, y2 = col[-1] # most recent point in column.
-                if abs(x2 - x1) < 15 and abs(y2 - y1) < 40:
-                    col.append((x1, y1))
-                    new_col = True
-                    break
-            if new_col == True:
-                new_col = False
-                grid.append([(x1, y1)])
+        return ret_pts
     
 
-        for col in grid:
-            copy = img.copy()
-            for pt in col:
-                cv.circle(copy, (pt[0], pt[1]), 9, (255,0,255), 2)
-            cv.imshow("img", copy)
-            cv.waitKey(0)    
+    def _answers(self, left_cols, right_cols, img):
+        self._get_blobs(img)
 
-            cv.circle(img, (pt[0], pt[1]), 9, (255,0,255), 2)
-            cv.imshow("img", img)
-            cv.waitKey(0)
+        # left_bubbles = self._merge_cols(left_cols)
+        # right_bubbles = self._merge_cols(right_cols)
+    
+    def _expected_amount(self, cols):
+        if cols is None:
+            return False
+
+        count = 0
+        for side in cols:
+            for col in side:
+                count += len(col)
+
+        return count == 46
+
+    def detect(self, img):
+        points = self._get_circlish(img)
+        grid = Grid(points)
+        side_cols = grid.side_most(2, output_img=img)
+
+        if self._expected_amount(side_cols):
+            left_cols, right_cols = side_cols
+            answers = self._answers(left_cols, right_cols, img)
+            print(answers)
