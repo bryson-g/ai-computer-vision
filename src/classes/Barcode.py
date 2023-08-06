b41aab744c68a71fccfb228b3aab0d4d4a979943
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-08-05 21:24:51 -0400

    add static return

--- src/classes/Barcode.py
+++ src/classes/Barcode.py
@@ -2,11 +2,13 @@ import cv2 as cv
 from pylibdmtx.pylibdmtx import decode
 
 class Barcode():
-    def scan(self, planar):
+
+    @staticmethod
+    def scan(planar):
         roi = planar[630:740, 170:290]
         cv.imshow("barcode_scan_roi", roi)
         
         codes = decode(roi)
         if codes:
             data = codes[0].data.decode('utf-8')
-            print(data)
+            return data
