fbdcf3e6c8f28dee482f1ca2cdc481c427672e5a
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-08-05 21:24:39 -0400

    set top most undistorted frame

--- src/classes/Card.py
+++ src/classes/Card.py
@@ -12,23 +12,24 @@ class Card():
     def __init__(self, **kwargs):
         self.projector = Projector()
         self.bubble_detector = BubbleDetector()
-        self.barcode = Barcode()
 
     def per_frame(self, src, copy, key_is):
         projection, undistorted = self.projector.planarize(copy)
+        cv.imshow("card_per_frame_undistorted", undistorted)
+        cv.setWindowProperty("card_per_frame_undistorted", cv.WND_PROP_TOPMOST, 1)
+
         if projection is None: return
         points, blobs = self.bubble_detector.detect(projection)
         grid = Grid(points, output_img=projection.copy())
 
         if len(grid.left) + len(grid.right) == 46:
             answers = Answers(grid.left, grid.right, blobs)
-            self.barcode.scan(projection)
+            data = Barcode.scan(projection)
+            print(data)
             print(answers.final)
 
         cv.imshow("card_per_frame_projection", projection)
-        cv.imshow("card_per_frame_undistorted", undistorted)
         zeros = np.zeros_like(projection)
         for pt in points:
             cv.circle(zeros, (int(pt[0]), int(pt[1])), 7, (0,255,0), 2)
-        
         cv.imshow("card_per_frame_zeros", zeros)
