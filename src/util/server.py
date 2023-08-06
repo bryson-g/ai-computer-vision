97350ec5a3fde27f3094d2f345da8ce7028d8529
Bryson G <114206517+bryson-g@users.noreply.github.com>
2023-08-05 21:25:21 -0400

    add support for url parameters

--- src/util/server.py
+++ src/util/server.py
@@ -1,35 +1,28 @@
 import http.server as serv
+from urllib.parse import urlparse, parse_qs
 from util.capture import save_frame
-import json
 
 # curl -X POST http://localhost:8080/api/picture -H "Content-Type: application/json" -d "{\"savePath\":\"C:/Users/bglembin/Desktop/test.jpg\"}"
 
 
 class RequestHandler(serv.BaseHTTPRequestHandler):
     def run(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        data = json.loads(post_data.decode('utf-8'))
-        print(data)
-        save_frame(data['savePath'])
+        parsed = urlparse(self.path)
+        params = parse_qs(parsed.query) 
+        save_frame(params['saveTo'][0])
 
     def do_POST(self):
-        if self.path == "/api/picture":
-            try:
-                self.run()
-                self.send_response(200)
-                self.end_headers()
-            except Exception as e:
-                print(e)
-                self.send_response(500)
-                self.end_headers()
-        else:
-            self.send_response(404)
+        try:
+            self.run()
+            self.send_response(200)
+            self.end_headers() 
+        except Exception as e:
+            print(e)
+            self.send_response(500)
             self.end_headers()
-            self.wfile.write("404 Not found".encode("utf-8"))
 
 def open_server():
-    server = serv.HTTPServer(("localhost", 8080), RequestHandler)
+    server = serv.HTTPServer(("localhost", 8094), RequestHandler)
     print("Server opened.")
 
     try:
